88c400c4f9106c1043b4d0a6d3a6d8adb2407671
Hugo Gu <hugogu@outlook.com>
2017-02-09 23:59:46 +0800

    Implements a simple symbolic differentition func.

--- SICPLearning/Main.fs
+++ SICPLearning/Main.fs
@@ -1,6 +1,8 @@
 ﻿open Sqrt
 open Rational
 open Stream
+open SymbolicDifferentition
+open Microsoft.FSharp.Linq.RuntimeHelpers
 
 [<EntryPoint>]
 let main argv = 
@@ -26,6 +28,12 @@ let main argv =
     let seg = make_segment (make_point 0.0 0.0) (make_point 3.0 6.0)
     printfn "%A" seg
     printfn "%A" (segment_mid seg)
+
+    let exp = <@ fun (x:float) -> x * x @>
+    let der = (derivExp exp)
+    printfn "deriv: %A" der
+    let derf = LeafExpressionConverter.EvaluateQuotation der :?> ( float -> float )
+    // printfn "deriv at 2.0: %A" (derf 2.0)
 
     let rect = make_rect 5.0 8.0
     printfn "round: %A" (rect_round rect)
---
+++ SICPLearning/SymbolicDifferentition.fs
@@ -0,0 +1,24 @@
+﻿module SymbolicDifferentition
+
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Quotations.DerivedPatterns
+
+let inline sum e1 e2 = <@ %e1 + %e2 @>
+let inline product e1 e2 = <@ %%e1 * %%e2 @>
+
+let rec deriv (exp:Expr) (var:Var) =
+    match exp with 
+    | Lambda(var, body) -> deriv body var
+    | Var(x) -> if x = var then <@ 1.0 @> else <@ 0.0 @>
+    | SpecificCall <@ (+) @> (_, _, [lArg;rArg]) -> sum (deriv lArg var) (deriv rArg var)
+    | SpecificCall <@ (*) @> (_, _, [lArg;rArg]) -> 
+        let pl =  product lArg (deriv rArg var)
+        let pr = product rArg (deriv lArg var)
+        sum pl pr
+    | _ -> invalidArg "exp" ("Invalid exp: " + exp.ToString())
+
+let derivExp (exp:Expr) = 
+  let x = Var("x", typeof<float>, false)
+  let derivBody = deriv exp x
+  <@ fun x -> %derivBody @>
